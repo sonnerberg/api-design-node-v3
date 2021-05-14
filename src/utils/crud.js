1542dc7f24f7ea354bab5f57e5b00ac97c242ed2
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-05-14 08:10:39 +0200

    Add solution for lesson 4

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,74 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const {
+    user: { _id: userId },
+    params: { id }
+  } = req
 
-export const getMany = model => async (req, res) => {}
+  const result = await model.findOne({ _id: id, createdBy: userId })
 
-export const createOne = model => async (req, res) => {}
+  if (!result) {
+    return res.status(404).end()
+  }
 
-export const updateOne = model => async (req, res) => {}
+  res.status(200).json({ data: result })
+}
 
-export const removeOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const {
+    user: { _id: userId }
+  } = req
+  const result = await model.find({ createdBy: userId })
+
+  res.status(200).json({ data: result })
+}
+
+export const createOne = model => async (req, res) => {
+  const {
+    user: { _id: userId },
+    body: { name }
+  } = req
+
+  const result = await model.create({ name, createdBy: userId })
+  res.status(201).json({ data: result })
+}
+
+export const updateOne = model => async (req, res) => {
+  const {
+    user: { _id: userId },
+    params: { id },
+    body: { name }
+  } = req
+
+  const result = await model.findOneAndUpdate(
+    { _id: id, createdBy: userId },
+    { name },
+    { new: true }
+  )
+
+  if (!result) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: result })
+}
+
+export const removeOne = model => async (req, res) => {
+  const {
+    params: { id },
+    user: { _id: userId }
+  } = req
+
+  const result = await model.findOneAndRemove({
+    _id: id,
+    createdBy: userId
+  })
+
+  if (!result) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: result })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
